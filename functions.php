b62aa36ca920d4433451c7e27121445a8f818417
Sascha-Darius Knießner <Dariuskniessner@littlewebgiants.com>
2017-10-21 23:23:40 +0200

    automatic commit

--- functions.php
+++ functions.php
@@ -2,6 +2,6 @@
 
 include_once 'inc/admin-menu.php';
 include_once 'inc/enqueue-scripts.php';
-include_once 'inc/create_post_types.php';
-include_once 'inc/create_taxonomies.php';
+//include_once 'inc/create_post_types.php';
+//include_once 'inc/create_taxonomies.php';
 include_once 'inc/mediathek_mod.php';
