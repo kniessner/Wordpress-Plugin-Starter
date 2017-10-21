dfe11cf0485fe75a886e3a8f72f6ff3121e90f50
Sascha-Darius Knießner <Dariuskniessner@littlewebgiants.com>
2017-10-21 22:21:03 +0200

    automatic commit

--- inc/enqueue-scripts.php
+++ inc/enqueue-scripts.php
@@ -3,9 +3,7 @@
 define ('STARTER_DEV_MODE', false);
 
 
-add_action('wp_enqueue_scripts', 'starter_enqueue_scripts');
-
-
+//add_action('wp_enqueue_scripts', 'starter_enqueue_scripts');
 function starter_enqueue_scripts(){
   $pathToDevJSFolder = substr(plugin_dir_path(__FILE__), 0, strlen(plugin_dir_path(__FILE__)) - 4 ) . 'dev/js/';
   $allJS = glob($pathToDevJSFolder . '*.js');
