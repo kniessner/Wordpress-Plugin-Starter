2633e8f9ffe2cc58da371861770312a6ac48b727
Sascha-Darius Knießner <Dariuskniessner@littlewebgiants.com>
2017-11-10 01:09:45 +0100

    option page

--- inc/admin-menu.php
+++ inc/admin-menu.php
@@ -18,3 +18,113 @@ function mt_toplevel_page_menu_call_back()
     echo $file;
   }
 }
+
+
+add_action( 'admin_menu', 'media_add_admin_menu' );
+add_action( 'admin_init', 'media_settings_init' );
+
+
+function media_add_admin_menu(  ) { 
+
+	add_options_page( 'complex', 'complex', 'manage_options', 'complex', 'media_options_page' );
+
+}
+
+
+function media_settings_init(  ) { 
+
+	register_setting( 'pluginPage', 'media_settings' );
+
+	add_settings_section(
+		'media_pluginPage_section', 
+		__( 'Your section description', 'complex' ), 
+		'media_settings_section_callback', 
+		'pluginPage'
+	);
+
+	add_settings_field( 
+		'media_text_field_0', 
+		__( 'Settings field description', 'complex' ), 
+		'media_text_field_0_render', 
+		'pluginPage', 
+		'media_pluginPage_section' 
+	);
+
+	add_settings_field( 
+		'media_radio_field_1', 
+		__( 'Settings field description', 'complex' ), 
+		'media_radio_field_1_render', 
+		'pluginPage', 
+		'media_pluginPage_section' 
+	);
+
+	add_settings_field( 
+		'media_select_field_2', 
+		__( 'Settings field description', 'complex' ), 
+		'media_select_field_2_render', 
+		'pluginPage', 
+		'media_pluginPage_section' 
+	);
+
+
+}
+
+
+function media_text_field_0_render(  ) { 
+
+	$options = get_option( 'media_settings' );
+	?>
+	<input type='text' name='media_settings[media_text_field_0]' value='<?php echo $options['media_text_field_0']; ?>'>
+	<?php
+
+}
+
+
+function media_radio_field_1_render(  ) { 
+
+	$options = get_option( 'media_settings' );
+	?>
+	<input type='radio' name='media_settings[media_radio_field_1]' <?php checked( $options['media_radio_field_1'], 1 ); ?> value='1'>
+	<?php
+
+}
+
+
+function media_select_field_2_render(  ) { 
+
+	$options = get_option( 'media_settings' );
+	?>
+	<select name='media_settings[media_select_field_2]'>
+		<option value='1' <?php selected( $options['media_select_field_2'], 1 ); ?>>Option 1</option>
+		<option value='2' <?php selected( $options['media_select_field_2'], 2 ); ?>>Option 2</option>
+	</select>
+
+<?php
+
+}
+
+
+function media_settings_section_callback(  ) { 
+
+	echo __( 'This section description', 'complex' );
+
+}
+
+
+function media_options_page(  ) { 
+
+	?>
+	<form action='options.php' method='post'>
+
+		<h2>complex</h2>
+
+		<?php
+		settings_fields( 'pluginPage' );
+		do_settings_sections( 'pluginPage' );
+		submit_button();
+		?>
+
+	</form>
+	<?php
+
+}
